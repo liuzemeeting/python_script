c5eb7884c40250b2c64cf86242e27cba037f2a67
liuzemeeting <15093095739@163.com>
2019-12-05 18:02:25 +0800

    wzh

--- demo.py
+++ demo.py
@@ -15,4 +15,19 @@ def dd(id):
 
 
 if __name__ == "__main__":
-    dd(sys.argv)
+    # dd(sys.argv)
+    a = 10
+    if a > 9:
+        print("1")
+    elif a > 8:
+        print("2")
+    elif a > 7:
+        print("12")
+    elif a > 6:
+        print("4")
+    elif a > 5:
+        print("15")
+    elif a > 4:
+        print("1666")
+    else:
+        print("fffffffffffffffff")
--- es_demo/get_data.py
+++ es_demo/get_data.py
@@ -1,7 +1,6 @@
 from sql_common import db
 
 
-
 def get_room_data():
     """
     获取房间数据
@@ -15,17 +14,13 @@ def get_room_data():
             ap.apartdesc,
             rm.roomno,
             rm.`name` room_name,
-            rm.area,
-            dr.roomno dr_roomno,
-            dr.`name` door_name,
-            dr.weigth door_weight,
-            dr.height  door height
+            rm.area
         FROM
             A_apart ap
             INNER JOIN A_room rm ON ap.apartno = rm.apartno
-            INNER JOIN A_door dr ON dr.roomno = rm.roomno
     """
     data = db.default.fetchall_dict(sql)
+    print("data", data)
     data_list = [obj.apartno for obj in data]
     data_list.sort()
     apart_data = []
@@ -34,23 +29,62 @@ def get_room_data():
         for item in data:
             if i == item["apartno"]:
                 value.append(item)
-        # d = {i: value}
-        # apart_data.append(d)
+        apart = {}
         rooms = []
+        door_dict = {}
+        toliet = []
+        cook_room = []
         for item in value:
+            # print(item)
             if item.room_name in ["主卧", "次卧", "儿童房", "大次卧", "客厅"]:
                 room = {}
-                door = []
                 room["area"] = item.area
-                room["door"] = {
-                    "height": item.door_height,
-                    "width": item.door_weigth
-                }
+                # room["door"] = get_door_data(item.roomno)
                 rooms.append(room)
-            if item.room_name in []:
-                pass
+            if item.room_name in ["卫生间"]:
+                tlt = {}
+                tlt["area"] = item.area
+                # tlt["door"] = get_door_data(item.roomno)
+                toliet.append(tlt)
+            if item.room_name in ["厨房"]:
+                ck = {}
+                ck["area"] = item.area
+                # ck["door"] = get_door_data(item.roomno)
+                cook_room.append(ck)
+        apart["rooms"] = rooms
+        apart["cook_room"] = cook_room
+        apart["toliet"] = toliet
+        d = {i: apart}
+        print(d)
+        apart_data.append(d)
 
     print(apart_data)
+
+
+def get_door_data(roomno):
+    """
+    获取房间内门的数据
+    :param roomno:
+    :return:
+    """
+    sql = """select doorno, roomno, width, height from A_door where roomno = %s""" % roomno
+    data = db.default.fetchall_dict(sql)
+    if data:
+        return data
+    return []
+
+
+def get_window_data(roomno):
+    """
+    获取房间内窗户的数据
+    :param roomno:
+    :return:
+    """
+    sql = """select doorno, roomno, width, height from A_door where roomno = %s""" % roomno
+    data = db.default.fetchall_dict(sql)
+    if data:
+        return data
+    return []
 
 
 if __name__ == '__main__':
